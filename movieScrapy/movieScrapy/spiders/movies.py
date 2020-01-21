05a681d80baebea0ee58a77a79ff054149bc4e71
jingjun <254032134@qq.com>
2020-01-21 17:22:39 +0800

    fix: scrapy 的select 解析

--- movieScrapy/movieScrapy/spiders/movies.py
+++ movieScrapy/movieScrapy/spiders/movies.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import lxml.etree as etree
+# import lxml.etree as etree
 import re
 from movieScrapy.items import MoviescrapyItem
 
@@ -11,16 +11,16 @@ class MoviesSpider(scrapy.Spider):
 
     def parse(self, response):
         items = MoviescrapyItem()
-        selector = etree.HTML(str(response.text))
-        movie_list = selector.xpath(
+        # selector = etree.HTML(str(response.text))
+        movie_list = response.xpath(
             '//div[@class="movielist"]/ul[@class="img-list clearfix"]/li')  # /a[@class="play-img"]/img/@alt
         for movie in movie_list:
-            movie_name = movie.xpath('./h5/a/text()')[0]
-            movie_url = movie.xpath('./h5/a/@href')[0]
-            actor = movie.xpath('./p')[0].xpath('./a/text()')
+            movie_name = movie.xpath('./h5/a/text()').extract_first()
+            movie_url = movie.xpath('./h5/a/@href').extract_first()
+            actor = movie.xpath('./p').xpath('./a/text()').extract()
             movie_actor = "/".join(map(lambda x: str(x), actor))
-            movie_star = movie.xpath('./p[@class="star"]/em/text()')[0]
-            movie_img = movie.xpath('./a[@class="play-img"]/img/@src')[0]
+            movie_star = movie.xpath('./p[@class="star"]/em/text()').extract_first()
+            movie_img = movie.xpath('./a[@class="play-img"]/img/@src').extract_first()
             items['movie_type'] = "动作片"
             items['movie_name'] = movie_name
             items['movie_url'] = movie_url
