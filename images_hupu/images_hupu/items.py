64bbdbec66d2a62122a712c32bf6da050ab32dac
jingjun <254032134@qq.com>
2020-02-18 17:35:06 +0800

    爬取帖子url与图片地址

--- images_hupu/images_hupu/items.py
+++ images_hupu/images_hupu/items.py
@@ -11,4 +11,6 @@ import scrapy
 class ImagesHupuItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    detail_url = scrapy.Field()
+    image_url = scrapy.Field()
--- images_hupu/images_hupu/spiders/hupu.py
+++ images_hupu/images_hupu/spiders/hupu.py
@@ -1,43 +1,54 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import requests
-import lxml.etree as etree
 from scrapy import Spider, Request
-
+from images_hupu.items import ImagesHupuItem
 
 class HupuSpider(scrapy.Spider):
     name = 'hupu'
     allowed_domains = ['bbs.hupu.com']
     start_urls = ['https://bbs.hupu.com/selfie']
+    base_url = 'https://bbs.hupu.com'
+    # def start_requests(self):
+    #     # 构建url
+    #     content = self.request()
+    #     links = self.xpath_action(content)
+    #     for url in links:
+    #         link = "https://bbs.hupu.com" + url
+    #         yield Request(url,self.parse)
 
     def start_requests(self):
-        # 构建url
-        content = self.request()
-        links = self.xpath_action(content)
-        for url in links:
-            link = "https://bbs.hupu.com" + url
-            yield Request(url,self.parse)
+        base_url = 'https://bbs.hupu.com/selfie-'
+        for page in range(1, 3):
+            url = base_url + str(page)
+            yield Request(url, self.parse)
 
     def parse(self, response):
-        pass
-
-    def request(self):
-        response =requests.get("https://bbs.hupu.com/selfie")
-        if response.status_code == 200:
-            return response.content
-        else:
-            return None
-
-    def xpath_action(self,content):
-        urls = []
-        select = etree.HTML(content)
-        list = select.xpath('//ul[@class="for-list"]/li')
-        if len(list) > 0:
-            list.remove(list[0])
-        for i in list:
-            link = i.xpath('./div[@class="titlelink box"]/a/@href')
+        lists = response.xpath('//ul[@class="for-list"]/li')
+        if len(lists) > 0:
+            lists.remove(lists[0])
+        for i in lists:
+            item = ImagesHupuItem()
+            link = i.xpath('./div[@class="titlelink box"]/a/@href').extract()
             if len(link) > 1:
-                urls.append(link(-1))
+                item["detail_url"] = link(-1)
             else:
-                urls.append(link[0])
-        return urls
+                item["detail_url"] = link[0]
+            url = self.base_url + item["detail_url"]
+            yield Request(url,meta={'item': item},callback=self.detail_parse)
+
+    def detail_parse(self,response):
+        # 接收上级已爬取的数据
+        item = response.meta['item']
+        imgurl = response.xpath('//div[@class="quote-content"]/p/img/@src').extract()
+        if "https://b1.hoopchina.com.cn/web/sns/bbs/images/placeholder.png" in imgurl:
+            imgurl.remove("https://b1.hoopchina.com.cn/web/sns/bbs/images/placeholder.png")
+        if len(imgurl) > 0:
+            long_url = ""
+            for i in imgurl:
+                img_url = i.split('?x',1)[0]
+                long_url = long_url + "," + img_url
+            item["image_url"] = long_url
+            yield item
+        else:
+            item["image_url"] = "null"
+            yield item
