17072449a3c55f4aa39a695ba212cad71dd668b9
jingjun <254032134@qq.com>
2020-02-20 18:01:44 +0800

    加入代理池

--- images_hupu/images_hupu/settings.py
+++ images_hupu/images_hupu/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'images_hupu.spiders'
 #USER_AGENT = 'images_hupu (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -52,9 +52,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'images_hupu.middlewares.ImagesHupuDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   # 'images_hupu.middlewares.ImagesHupuDownloaderMiddleware': 543,
+    'images_hupu.middlewares.ProxyMiddleware': 543
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -70,6 +71,8 @@ ITEM_PIPELINES = {
 }
 
 IMAGES_STORE = './images'
+PROXY_POOL_URL = 'http://localhost:5000/random'
+PROXY_URL = 'http://localhost:5000/random'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -94,3 +97,5 @@ IMAGES_STORE = './images'
 
 MONGO_URI = 'localhost'
 MONGO_DB = 'hupu'
+
+RETRY_HTTP_CODES = [401, 403, 408, 414, 500, 502, 503, 504]
